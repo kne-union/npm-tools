aa0975f11a365011fc224442049af131bff48fa8
zhipenglin <sunandmoon001@163.com>
2024-03-20 14:06:14 +0800

    更新初始化项目脚手架操作逻辑

--- bin.js
+++ bin.js
@@ -4,7 +4,7 @@ const npmTool = require('./index');
 const fs = require('fs-extra');
 const path = require('path');
 const downloadNpmPackage = require('@kne/fetch-npm-package');
-
+const {select} = require('@inquirer/prompts');
 const args = process.argv.slice(2);
 
 const script = args[0];
@@ -70,9 +70,23 @@ switch (script) {
         if (!args[1]) {
             throw new Error('项目名不能缺省');
         }
-        npmTool.initProject(args[1], ...(args[2] ? args[2].split(/(?<!^)@/) : ['@kne-template/node'])).catch((err) => {
-            throw err;
-        });
+        (async () => {
+            const templateArgs = args[2] ? args[2].split(/(?<!^)@/) : [await select({
+                message: '请选择模板类型', choices: [{name: 'NodeJS Libs', value: '@kne-template/node'}, {
+                    name: 'Frontend Libs', value: '@kne-template/libs'
+                }, {name: 'Remote Components', value: '@kne-template/remote'}, {
+                    name: 'Business Project', value: '@kne-template/project'
+                }, {
+                    name: 'WeChat Miniprogram Libs', value: '@kne-template/miniprogram-libs'
+                }, {
+                    name: 'WeChat Miniprogram Project', value: '@kne-template/miniprogram-project'
+                }],
+            })];
+
+            npmTool.initProject(args[1], ...templateArgs).catch((err) => {
+                throw err;
+            });
+        })();
 }
 
 
